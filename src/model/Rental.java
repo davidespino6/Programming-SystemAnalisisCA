24b4e6a4da1c2389c439b9716a53d3ddb0706547
davidespino6 <davidespino6@gmail.com>
2021-05-11 15:29:16 +0100

    This is my project. David Espino

---
+++ src/model/Rental.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author davidespino
+ */
+public class Rental {
+    
+    private String rent;
+    private String returN;
+    private int payment;
+    private String receipt;
+    private int total;
+    private int charges1;
+    
+}
